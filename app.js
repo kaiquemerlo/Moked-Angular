a5b3b04ab42df4e6cb585464ad845d31647f8092
akaFTS <gustavo84171@hotmail.com>
2016-05-14 11:48:09 -0300

    Atualizado.

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
 app = angular.module("moked", []);
 
-app.controller("LembretesCtrl", function($scope){
+app.controller("LembretesCtrl", function($scope, buscarService){
+    $scope.lembretes = [];
 
+    var lembrete = {};
+    lembrete.nome = "Comprar pão";
+    lembrete.desc = "Lembrar de ir na padaria";
+    lembrete.data = "14/05/2016";
+    lembrete.importante = false;
+    $scope.lembretes.push(lembrete);
+
+    lembrete = {};
+    lembrete.nome = "Açougue";
+    lembrete.desc = "Comprar carne";
+    lembrete.data = "14/05/2016";
+    lembrete.importante = true;
+    $scope.lembretes.push(lembrete);
+
+
+    $scope.buscarTxt = function() {
+        buscarService.buscar().then(function(valor){
+            alert("oi funcionou "+valor);
+        });
+        alert("tchau");
+    }
 });
 
 app.service("somaService", function(){
     this.somar = function(a, b){
         return a + b;
+    }
+});
+
+app.service("buscarService", function($http, $q){
+    this.buscar = function(){
+        var promise = $q.defer();
+        
+        $http.get("buscar.txt").then(function(data){
+            promise.resolve(data.data);
+        }, function(error){
+            promise.reject(error);
+        });
+
+        return promise.promise;
     }
 });
